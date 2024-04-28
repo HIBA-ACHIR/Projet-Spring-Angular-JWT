fdab86bdd65152e6f6d1cba46fe895cde94dd055
Hibatallah Achir <hibatallahachir@gmail.com>
2024-04-28 16:02:44 +0100

    update

---
+++ src/test/java/org/hiba/ebankingbackend/EbankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hiba.ebankingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
